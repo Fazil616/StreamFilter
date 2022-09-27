25f623fae8d27c13c37d89a6f34f57e072e9aeac
Fazil616 <fazil616shaikh@gmail.com>
2022-09-27 14:00:19 +0530

    Initial commit

---
+++ src/com/java/features/stream/FilterExample.java
@@ -0,0 +1,43 @@
+package com.java.features.stream;
+import java.util.ArrayList;
+import java.util.List;
+public class FilterExample {
+    public static void main (String[]args) {
+        //create list of Products
+        List<Product> list = new ArrayList<Product>();
+        list.add(new Product(1, "Rose", 500));
+        list.add(new Product(2, "Lily", 1000));
+        list.add(new Product(3, "Lotus", 1500));
+        list.add(new Product(4, "Jasmine", 2000));
+        list.add(new Product(5, "Hibiscus", 3000));
+        list.add(new Product(6, "Exora", 3500));
+        //filter price <2000
+        list.stream().filter(p -> p.price < 2000)
+                .forEach(System.out::println);
+        //filter Starts from"L"
+        list.stream().filter(product -> product.name.startsWith("L"))
+                .forEach(ele -> System.out.println(ele));
+        //multiple filter
+        System.out.println("multiple filter");
+        list.stream()
+                .filter(p -> p.price > 2000)
+                .filter(product -> product.name.startsWith("L"))
+                .forEach(System.out::println);
+    }
+}
+class Product {
+    int productid;
+    String name;
+    double price;
+
+    public Product(int productid, String name, double price) {
+        this.productid = productid;
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "product [productid=" + productid + ",name=" + name + ", price=" + price + "]";
+    }
+}
